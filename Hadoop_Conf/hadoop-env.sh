0f4535427ceaa51f0d8c91ca6e4a81c32156752e
Chisha14 <chisha_7@hotmail.com>
2017-06-07 16:43:23 +0200

    Add files via upload

--- Hadoop_Conf/hadoop-env.sh
+++ Hadoop_Conf/hadoop-env.sh
@@ -19,7 +19,7 @@
 # set JAVA_HOME in this file, so that it is correctly defined on
 # remote nodes.
 # The java implementation to use.
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 # The jsvc implementation to use. Jsvc is required to run secure datanodes
 # that bind to privileged ports to provide authentication of data transfer
 # protocol.  Jsvc is not required if SASL is configured for authentication of
